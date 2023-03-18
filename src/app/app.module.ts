e1016980e5272cf4a4dfb79c949fed3a9f0e55af
hugo1623 <hugoluis.system@gmail.com>
2023-03-18 15:37:04 -0500

    Create Component Discs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DiscsComponent } from './pages/discs/discs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    DiscsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/discs/discs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discs',
+  templateUrl: './discs.component.html',
+  styleUrls: ['./discs.component.css']
+})
+export class DiscsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
